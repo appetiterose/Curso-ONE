fc36e593847a3955961441e856be95fd54bf3c7c
appetiterose <wepdaniel@gmail.com>
2023-03-29 22:46:42 -0600

    Primer clase de JS Herencias e Interfaces

---
+++ JS Herencia e Interfaces en Orientacion a Objetos/CuentaCorriente.js
@@ -0,0 +1,11 @@
+import { Cliente } from "./Cliente.js";
+import { Cuenta } from "./Cuenta.js";
+
+export class CuentaCorriente extends Cuenta{
+    static cantidadCuentas = 0;
+    
+    constructor(cliente, numero, agencia) {
+        super(cliente,numero,agencia,0);
+        CuentaCorriente.cantidadCuentas++;
+    }
+}
